790ff5c3dde45ef16a693be28a908e52e8086f5a
VincenzoManfredi99 <vincenzomanfredi2050@gmail.com>
2026-03-25 14:37:41 +0100

    Pulizia codice

--- script.js/scriptBenchmark.js
+++ script.js/scriptBenchmark.js
@@ -62,7 +62,7 @@ nextBtn.addEventListener('click', ()=>{
          if (currentQuestionIndex < databaseQuestions.length){
              showQuestion()
              resetTimer()
-         } else {                                                        //Con questo else verranno mostrati i risultati
+         } else {                                              //Con questo else verranno mostrati i risultati
             mostraPaginaRisultati()
 }
 })
@@ -100,7 +100,7 @@ const timer = ()=> {
             resetTimer()
             timer()
             }
-    }, 1000)                                                    //Set interval funziona con i milliseconds, da specificare quindi che l'azione va compita ogni secondo
+    }, 1000)                             //Set interval funziona con i milliseconds, da specificare quindi che l'azione va compita ogni secondo
 }
 
 timer()
